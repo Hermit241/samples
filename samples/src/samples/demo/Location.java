28ae95a0be65dbaf5e26309aa90bd699c5b058af
Hermit241 <81275243+Hermit241@users.noreply.github.com>
2022-07-27 19:41:09 +0800

    Location.java commit

---
+++ samples/src/samples/demo/Location.java
@@ -0,0 +1,13 @@
+package samples.demo;
+
+public class Location {
+	private int location;
+	
+	int getLocation() {
+		return location;
+	}
+	
+	void setLocation(int location) {
+		this.location = location;
+	}
+}
